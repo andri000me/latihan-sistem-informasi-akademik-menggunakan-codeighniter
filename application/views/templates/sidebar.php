b0601b56bf11e6db707e792e432bc67286da09cf
Diyar.codes <diyar.codes16@gmail.com>
2020-01-25 20:02:56 +0700

    memperbaiki template

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,73 +1,67 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="<?= base_url('assets/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">SIAKAD</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="<?= base_url('assets/') ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
-
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-graduation-cap"></i>
-                        <p>Mahasiswa</p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
-                        <p>Dosen</p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-book"></i>
-                        <p>Jadwal Kuliah</p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-university"></i>
-                        <p>Jurusan</p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-lock"></i>
-                        <p>Ganti Password</p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-sign-out-alt"></i>
-                        <p>Logout</p>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-</aside>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+      <!-- sidebar: style can be found in sidebar.less -->
+      <section class="sidebar">
+          <!-- Sidebar user panel -->
+          <div class="user-panel">
+              <div class="pull-left image">
+                  <img src="<?= base_url('assets/'); ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+              </div>
+              <div class="pull-left info">
+                  <p>Alexander Pierce</p>
+                  <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+              </div>
+          </div>
+          <!-- search form -->
+          <form action="#" method="get" class="sidebar-form">
+              <div class="input-group">
+                  <input type="text" name="q" class="form-control" placeholder="Search...">
+                  <span class="input-group-btn">
+                      <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                      </button>
+                  </span>
+              </div>
+          </form>
+          <!-- /.search form -->
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu" data-widget="tree">
+              <li class="header">MAIN NAVIGATION</li>
+              <li class="active treeview">
+                  <a href="#">
+                      <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                  </a>
+              </li>
+              <li class="active treeview">
+                  <a href="#">
+                      <i class="fa fa-graduation-cap"></i> <span>Mahasiswa</span>
+                  </a>
+              </li>
+              <li class="active treeview">
+                  <a href="#">
+                      <i class="fa fa-user"></i> <span>Dosen</span>
+                  </a>
+              </li>
+              <li class="active treeview">
+                  <a href="#">
+                      <i class="fa fa-book"></i> <span>Jadwal Kuliah</span>
+                  </a>
+              </li>
+              <li class="active treeview">
+                  <a href="#">
+                      <i class="fa fa-university"></i> <span>Jurusan</span>
+                  </a>
+              </li>
+              <li class="active treeview">
+                  <a href="#">
+                      <i class="fa fa-lock"></i> <span>Ganti Password</span>
+                  </a>
+              </li>
+              <li class="active treeview">
+                  <a href="#">
+                      <i class="fa fa-sign-out"></i> <span>Logout</span>
+                  </a>
+              </li>
+          </ul>
+      </section>
+      <!-- /.sidebar -->
+  </aside>
